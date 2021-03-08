347b08eded039f44a143e6b46067b2c180d7bc64
mpuch12 <45654602+mpuch12@users.noreply.github.com>
2021-03-08 16:47:55 +0100

    Allow to change company address

--- src/main/java/pl/umk/mat/booking/service/AdminService.java
+++ src/main/java/pl/umk/mat/booking/service/AdminService.java
@@ -1,14 +1,9 @@
 package pl.umk.mat.booking.service;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.umk.mat.booking.model.CompanyDetails;
 import pl.umk.mat.booking.repository.CompanyDetailsRepository;
 import pl.umk.mat.booking.repository.EmployeeRepository;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 @Service
 public class AdminService {
@@ -27,11 +22,10 @@ public class AdminService {
     public void updateCompanyDetails(CompanyDetails companyDetails, String field) {
         CompanyDetails savedCompanyDetails = companyDetailsRepository.findById(1);
 
-        switch (field){
-            case "name":
-            savedCompanyDetails.setName(companyDetails.getName());
-            companyDetailsRepository.save(savedCompanyDetails);
-            break;
+        switch (field) {
+            case "name" -> savedCompanyDetails.setName(companyDetails.getName());
+            case "address" -> savedCompanyDetails.setAddress(companyDetails.getAddress());
+            default -> companyDetailsRepository.save(savedCompanyDetails);
         }
     }
 }
